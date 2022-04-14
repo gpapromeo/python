25d64b8e3c4d2563eeb898b2e76bd2925f7870ef
romeo <romeo.casesa@gpadvancedprojects.com>
2022-04-14 17:08:34 +0200

    Initialize file

---
+++ RL_simulation.py
@@ -0,0 +1,10 @@
+"""
+Author:         Romeo Casesa
+First issue:    14-04-2022
+Last modified:  14-04-2022
+
+This file simulates the behaviour of the satellite and the data reception on ground.
+"""
+
+import pandas as pd
+import datetime
